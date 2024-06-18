aadcb8f2f8976345b1773bc6f07a8abb33dd4738
ESedrak <elizabeth_sedrak@hotmail.com>
2024-06-18 13:26:22 +1000

    feat: add circuit breaker to apigateway and order service

--- order-service/src/main/java/shopping/app/orderservice/client/InventoryClient.java
+++ order-service/src/main/java/shopping/app/orderservice/client/InventoryClient.java
@@ -1,10 +1,19 @@
 package shopping.app.orderservice.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
 public interface InventoryClient {
 
     @GetExchange("/api/inventory")
-    boolean isInStock(@RequestParam String skuCode, @RequestParam int quantity);
+    @CircuitBreaker(name="inventory", fallbackMethod = "fallbackMethod")
+    @Retry(name="inventory")
+    boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+
+    default boolean fallbackMethod(String skuCode, Integer quantity, Throwable ex) {
+        System.out.println("Cannot get inventory for sku code: " + skuCode + ", error reason: " + ex.getMessage());
+        return false;
+    }
 }
